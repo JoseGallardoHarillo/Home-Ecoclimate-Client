1c8e9517acfd7020b71a6c16fc1b09ac95a2144d
Jose Gallardo Harillo <115019998+JoseGallardoHarillo@users.noreply.github.com>
2024-11-28 22:36:43 +0100

    Update ServoWB.h

--- src/drivers/ServoWB.h
+++ src/drivers/ServoWB.h
@@ -10,15 +10,15 @@ class ServoWB : public WindowBlind {
 
     public:
         /**
-         * @param pin Número de pin al cual la señal del servo está conectado
+         * @param pin Pin number to which the servo signal is connected
          */
         ServoWB(uint8_t pin);
         /**
-         * @param pin Número de pin al cual la señal del servo está conectado
-         * @param min Ancho del pulso, en microsegundos, correspondiente al
-         *            ángulo mínimo en el servo (0º)
-         * @param max Ancho del pulso, en microsegundos, correspondiente al
-         *            ángulo máximo en el servo (180º)
+         * @param pin Pin number to which the servo signal is connected
+         * @param min Pulse width, in microseconds, corresponding to the
+         *            minimum angle of the servo (0º)
+         * @param max Pulse width, in microseconds, corresponding to the
+         *            maximum angle of the servo (180º)
          */
         ServoWB(uint8_t pin, uint16_t min, uint16_t max);
         void setAngle(uint8_t angle);
